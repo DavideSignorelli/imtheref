abcbf78ffc963ea8fa9769d3703a093d829f6a10
signo <you@example.com>
2024-05-14 12:55:48 +0200

    incassi

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,8 +1,41 @@
 import Tabella from "../components/tabella";
 import Typography from '@mui/joy/Typography';
 import Sheet from '@mui/joy/Sheet';
+import { useState, useEffect } from 'react';
+
+async function getDati() {
+    if (window.location.pathname === "/home") {
+        const response = await fetch("/api/partita/visualizza", {
+            credentials: "include"
+        });
+        return await response.json();
+    }
+}
 
 function home() {
+    const [dati, setDati] = useState([]);
+    const [totaleIncassi, setTotaleIncassi] = useState(0);
+    const [incassato, setIncassato] = useState(0);
+    const [daIncassare, setDaIncassare] = useState(0);
+
+    useEffect(() => {
+        getDati().then((dati) => {
+            setDati(dati);
+        });
+    }, []);
+
+    useEffect(() => {
+        let totale = 0;
+        let incassato = 0;
+        dati.forEach((partita) => {
+            totale += partita.rimborso;
+            partita.incasso == true ? incassato += partita.rimborso : null;
+        });
+        setTotaleIncassi(totale);
+        setIncassato(incassato);
+        setDaIncassare(totale - incassato);
+    }, [dati]);
+
     return (
         <div>
             <Sheet
@@ -23,6 +56,15 @@ function home() {
                     IMTHEREF
                 </Typography>
                 <Tabella />
+                <Typography level="h4" component="h4" textAlign={"left"}>
+                    Totale rimborsato: {totaleIncassi}€
+                </Typography>
+                <Typography level="h4" component="h4" textAlign={"left"}>
+                    Rimborsi incassati: {incassato}€
+                </Typography>
+                <Typography level="h4" component="h4" textAlign={"left"}>
+                    Rimborsi da incassare: {daIncassare}€
+                </Typography>
             </Sheet>
         </div>
     );
