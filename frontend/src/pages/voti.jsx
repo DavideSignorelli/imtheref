21a4e02a7d5083a60a8080ebb256e1d22543dc7c
signo <you@example.com>
2024-05-15 09:22:12 +0200

    pagine dei grafici ritoccate

--- frontend/src/pages/voti.jsx
+++ frontend/src/pages/voti.jsx
@@ -1,10 +1,28 @@
 import GraficoVoti from "../components/graficovoti";
+import Typography from '@mui/joy/Typography';
+import Sheet from '@mui/joy/Sheet';
+
 
 export default function Voti() {
     return (
-        <div>
-            <h1>Voti</h1>
+        <Sheet
+            sx={{
+                width: "70%",
+                mx: 'auto', // margin left & right
+                my: 4, // margin top & bottom
+                py: 3, // padding top & bottom
+                px: 2, // padding left & right
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+                borderRadius: 'sm',
+                boxShadow: 'md',
+            }}
+        >
+            <Typography level="h1" component="h1" textAlign={"center"}>
+                VALUTAZIONI
+            </Typography>
             <GraficoVoti />
-        </div>
+        </Sheet>
     );
 }
